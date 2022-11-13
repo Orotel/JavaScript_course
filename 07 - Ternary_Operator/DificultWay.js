5fd917769dce8bc49e2dc9cfe600eda6670a4023
Orotel <orotel.aaos@gmail.com>
2022-11-13 17:40:29 -0300

    add Ternary Operator

---
+++ 07 - Ternary_Operator/DificultWay.js
@@ -0,0 +1,21 @@
+// Case (1) 
+let num = 5;
+if (num ===7) {
+    console.log("lucky");
+}
+else{
+    console.log("Bad!!")
+}
+
+// Case (2)
+let stats ="offline"
+
+let color = ""
+if(stats === "offline"){
+    color ="RED"
+    
+} 
+else{
+    color ="GREEN"
+}
+console.log(color)
---
+++ 07 - Ternary_Operator/EasyWay.js
@@ -0,0 +1,9 @@
+//Condition ? expIFTrue : expIFfALSE
+
+// Case (1) 
+let num = 5;
+num===7 ? console.log("Lucky"):console.log("Bad!!")
+
+// Case (2)
+stats = "of"
+let color = stats ==="offline" ?  "Red" :  "Green"
