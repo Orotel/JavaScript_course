07a2143634a92e7d503c2e831495ca923f6cc276
Orotel <orotel.aaos@gmail.com>
2022-11-13 15:36:11 -0300

    add Logical Operators

---
+++ 04 - LogicalOperators/OR_(||)/ex7.js
@@ -0,0 +1,12 @@
+// ----AllTrue 
+let TrueTrue = 1 === 1 || 10 === 10;
+    console.log(TrueTrue)
+
+// ----Onefalse
+let FalseTrue = 1 === 1 || null;
+    console.log(FalseTrue)
+
+// ----AllFalse
+let FalseFalse = 0 || 10 === "10";
+    console.log(FalseFalse)
+    
--- Truty&Falsy/ex4.js
+++
@@ -1,9 +0,0 @@
-let mistery = 56;
-
-if(mistery){
-    console.log("Truthy");
-}
-else{//is Falsy if value === :"",false,null,undefined,NaN//
-    console.log("Falsy");
-
-}
